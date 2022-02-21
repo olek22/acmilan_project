9e738a78eed813327877dea8bb3ec3cc590e7cff
olek22 <golaolek@gmail.com>
2022-02-21 13:26:21 +0100

    user interface
    changes in db + user interface (CRUD) in all_clubs view

--- acmilanweb/urls.py
+++ acmilanweb/urls.py
@@ -4,8 +4,8 @@ from .views import all_clubs, new_club, update_club, delete_club
 
 
 urlpatterns = [
-    path('seriea_clubs/', all_clubs),
-    path('seriea_clubs/create', new_club),
-    path('seriea_clubs/update/<int:id>/', update_club),
-    path('seriea_clubs/delete/<int:id>/', delete_club),
+    path('seriea_clubs/', all_clubs, name="all_clubs"),
+    path('seriea_clubs/create', new_club, name="create_club"),
+    path('seriea_clubs/update/<int:id>/', update_club, name="update_club"),
+    path('seriea_clubs/delete/<int:id>/', delete_club, name="delete_club"),
 ]
