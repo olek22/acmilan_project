adf4a6a6bed952bfbeeb52413d7edbaac7fa3ed3
olek22 <golaolek@gmail.com>
2022-02-23 20:21:36 +0100

    images

--- acmilanweb/static/js/controllers.js
+++ acmilanweb/static/js/controllers.js
@@ -6,27 +6,27 @@ portfolioApp.controller('GalleryListCtrl', function($scope)
     [
         { 'title':'Ateny',
         'when':'Maj 2007',
-        'thumbnailUrl':'img/2007.jpg'
+        'thumbnailUrl':'/img/2007.jpg'
         },
         { 'title':'Manchester',
         'when':'Maj 2003',
-        'thumbnailUrl':'img/2003.jpg'
+        'thumbnailUrl':'/media/img/2003.jpg'
         },
         { 'title':'Ateny',
         'when':'Maj 1994',
-        'thumbnailUrl':'img/1994.jpg'
+        'thumbnailUrl':'/media/img/1994.jpg'
         },
         { 'title':'Wieden',
         'when':'Maj 1990',
-        'thumbnailUrl':'img/1990.jpg'
+        'thumbnailUrl':'/media/img/1990.jpg'
         },
         { 'title':'Barcelona',
         'when':'Maj 1989',
-        'thumbnailUrl':'img/1989.jpg'
+        'thumbnailUrl':'my_media/img/1990.jpg'
         },
         { 'title':'Madryt',
         'when':'Maj 1969',
-        'thumbnailUrl':'img/1969.jpg'
+        'thumbnailUrl':'./my_media/img/1969.jpg'
         }
     ];
     $scope.sortList = 
