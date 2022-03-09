91e214a2245f463485c94fb90f1189a9bd840e7c
olek22 <golaolek@gmail.com>
2022-03-09 14:29:13 +0100

    add views to set forgetten password

--- acmilan/settings.py
+++ acmilan/settings.py
@@ -131,3 +131,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'main'
 LOGOUT_REDIRECT_URL = 'main'
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'golatestapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'Test123!'
