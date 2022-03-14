0c80e05294dd064a0835bdbf355df871576f5e81
olek22 <golaolek@gmail.com>
2022-03-14 12:01:18 +0100

    change language to english

--- acmilanweb/static/js/controllers.js
+++ acmilanweb/static/js/controllers.js
@@ -5,43 +5,43 @@ portfolioApp.controller('GalleryListCtrl', function($scope)
 {
     $scope.galleries = 
     [
-        { 'title':'Ateny',
-        'when':'Maj 2007',
+        { 'title':'Athens',
+        'when':'May 2007',
         'thumbnailUrl':'/media/img/2007.jpg'
         },
         { 'title':'Manchester',
-        'when':'Maj 2003',
+        'when':'May 2003',
         'thumbnailUrl':'/media/img/2003.jpg'
         },
-        { 'title':'Ateny',
+        { 'title':'Athens',
         'when':'Maj 1994',
         'thumbnailUrl':'/media/img/1994.jpg'
         },
-        { 'title':'Wieden',
-        'when':'Maj 1990',
+        { 'title':'Vienna',
+        'when':'May 1990',
         'thumbnailUrl':'/media/img/1990.jpg'
         },
         { 'title':'Barcelona',
         'when':'Maj 1989',
         'thumbnailUrl':'/media/img/1990.jpg'
         },
-        { 'title':'Madryt',
-        'when':'Maj 1969',
+        { 'title':'Madrid',
+        'when':'May 1969',
         'thumbnailUrl':'/media/img/1969.jpg'
         }
     ];
     $scope.sortList = 
     [
         {
-            'label':'alfabetycznie',
+            'label':'alphabetic',
             'value':'title'
         },
         {
-            'label':'chronologicznie',
+            'label':'chronological',
             'value':'when'
         },
         {
-            'label':'od najnowszych',
+            'label':'latest',
             'value':'-when'
         },
     ];
