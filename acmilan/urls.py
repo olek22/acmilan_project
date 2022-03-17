ea91b27359713fd83ddfc6f9263d2442e6e558eb
olek22 <golaolek@gmail.com>
2022-03-17 15:46:24 +0100

    change in settings to redirect

--- acmilan/urls.py
+++ acmilan/urls.py
@@ -18,8 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='acmilan/')),
     path('admin/', admin.site.urls),
     path('acmilan/', include('acmilanweb.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
